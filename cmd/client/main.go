1357008423f2f0f6ac3d06230a0d98ea127b154e
Tikaram Mardi <tikarammardi007@gmail.com>
2026-02-13 18:12:56 +0530

    add main application logic and update go.mod with dependencies

---
+++ cmd/client/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"rate-limiter-engine/proto"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	// 1. Establish a connection to the server
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := proto.NewRateLimiterClient(conn)
+
+	// 2. Simulate 5 quick requests for "user_123"
+	userID := "user_123"
+	for i := 1; i <= 5; i++ {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
+
+		r, err := c.Check(ctx, &proto.LimitRequest{UserId: userID, Key: "login"})
+		if err != nil {
+			log.Fatalf("could not check: %v", err)
+		}
+
+		log.Printf("Request %d: Allowed = %v", i, r.Allowed)
+
+		// Optional: slight delay to see it happen in real-time
+		time.Sleep(100 * time.Millisecond)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module rate-limiter-engine
 
-go 1.24
+go 1.24.0
+
+toolchain go1.24.11
+
+require (
+	google.golang.org/grpc v1.79.1
+	google.golang.org/protobuf v1.36.11
+)
+
+require (
+	golang.org/x/net v0.48.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
+	golang.org/x/text v0.32.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251202230838-ff82c1b0f217 // indirect
+)
